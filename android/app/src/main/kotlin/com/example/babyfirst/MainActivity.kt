e9f02bc2da88824898a044f6090f2b0d90fd9ee2
victor <dvicxy@gmail.com>
2022-01-04 11:16:29 +0100

    first commit

---
+++ android/app/src/main/kotlin/com/example/babyfirst/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.babyfirst
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/models/constants.dart
@@ -0,0 +1,3 @@
+import 'package:flutter/material.dart';
+
+const buttons = Color(0xff234512);
